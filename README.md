f4dff089cccbf01f3260c634ecb0ee76d5bc65ad
dengdc <2223445830@qq.com>
2017-12-12 21:07:15 +0800

    ok

---
+++ README.md
@@ -0,0 +1,3 @@
+## poor-guy
+
+对Laravel Artisan 命令行工具的抽离
--- routes/console.php
+++ routes/console.php
@@ -1,2 +1,6 @@
 <?php
 
+/** @var  $artisan \App\Console\Kernel */
+$artisan->command("email:send {user}", function($user){
+    $this->info(" send {$user}");
+});
